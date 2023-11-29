8c63e2dda5b04b9b7d8d52e1b8b0b6b460f7b86f
Gilberto-Guzman <josegilbertoguzmangutierrez@gmail.com>
2023-11-29 01:29:19 -0600

    Uso de GitHub Copilot con JavaScript

---
+++ 8. Uso de GitHub Copilot con JavaScript/src/App.jsx
@@ -0,0 +1,53 @@
+/**
+ * Application component
+ *
+ * To contain application wide settings, routes, state, etc.
+ */
+
+import React from "react";
+
+import About from "./Components/About";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Portfolio from "./Components/Portfolio";
+
+import "./styles.css";
+
+/**
+ * This object represents your information. The project is set so that you
+ * only need to update these here, and values are passed a properties to the
+ * components that need that information.
+ *
+ * Update the values below with your information.
+ *
+ * If you don't have one of the social sites listed, leave it as an empty string.
+ */
+const siteProps = {
+  name: "Gilberto Guzmán",
+  title: "Desarrollador de software",
+  email: "josegilbertoguzmangutierrez@gmail.com",
+  gitHub: "https://github.com/Gilberto-Guzman",
+  instagram: "1",
+  linkedIn: "2",
+  medium: "3",
+  twitter: "4",
+  youTube: "5",
+};
+
+const primaryColor = "#4E567E";
+const secondaryColor = "#D2F1E4";
+
+const App = () => {
+  return (
+    <div id="main">
+      <Header />
+      <Home name={siteProps.name} title={siteProps.title} />
+      <About />
+      <Portfolio />
+      <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
+    </div>
+  );
+};
+
+export default App;
